bd1a8edd2204c0c17fc417626cba70f95b376218
joel <joelsosayaquispe@gmail.com>
2022-07-17 04:06:53 -0500

    joe

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import { BrowserRouter ,Routes,Route} from 'react-router-dom';
 import  HomePage from './pages/homepage';
 function App() {
   return (
-    <BrowserRouter Basename={process.env.PUBLIC_URL}>
+    
       <Routes>
-        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/portafolio" element={<HomePage/>}></Route>
       </Routes>
-    </BrowserRouter>
+   
   );
 }
 
